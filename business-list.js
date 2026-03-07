89de5231de3844f1e0c5fbaa845b6e386ed3d78a
Abdelhak <tigha66@gmail.com>
2026-03-07 19:12:43 +0000

    Updated WhatsApp dashboard with 35 businesses from here.now list

---
+++ business-list.js
@@ -0,0 +1,15 @@
+// Business list for WhatsApp outreach
+const businesses = [
+    // Existing with websites
+    {name:"Englands Plumber",cat:"Plumber",site:"englands-plumber/",phone:"07405881006"},
+    {name:"Tubbs Plumbing",cat:"Plumber",site:"tubbs-plumbing/",phone:"02086959165"},
+    {name:"Fast Plumber London",cat:"Plumber",site:"fast-plumber-london/",phone:"02078711956"},
+    {name:"National Plumbers",cat:"Plumber",site:"national-plumbers/",phone:"07760244444"},
+    {name:"MC Plumber",cat:"Plumber",site:"mc-plumber/",phone:"02079460194"},
+    {name:"East London Plumbing",cat:"Plumber",site:"east-london-plumbing/",phone:"02039848457"},
+    {name:"Bens Plumbing",cat:"Plumber",site:"bens-plumbing/",phone:"02086946618"},
+    {name:"Plumber 24hour London",cat:"Plumber",site:"plumber-24hour-london/",phone:""},
+    {name:"Emergency Plumber London",cat:"Plumber",site:"emergency-plumber-london/",phone:""},
+    {name:"Plumbing Circle",cat:"Plumber",site:"plumbing-circle/",phone:"07867850846"},
+    // Add more from here.now list - 300+ businesses without sites
+];
